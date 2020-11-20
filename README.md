60e6520b11854b7bb104902261bb3e359ee4982c
nartexyu <70301421+nartexyu@users.noreply.github.com>
2020-11-19 21:47:06 -0800

    completed script: forecast now populates data, search history added, localStorage features added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Weather-Dashboard
+   
+This weather dashboard has an input box where the user can input a city to retreive the current weather information as well as the next five day forecast. The current weather information will contain an icon representing the weather, temperature in Fahrenheight, humidity %, wind speed in MPH, and UV-index. The forecasted weather information will include the same information except the wind speed and UV-index. If the UV-index is less than 3, it will be displayed in a green box for favorable; if it is greater than 3 but less than 6 it will be displayed in a yellow box for moderate; if it is greater than 6, 
+it will be displayed in a red box for severe. 
+
+With each searched city, the city will be appended to a list below to as representation of the users search history. If the user wishes to review the city's weather, the user may click on the box in the search history to review the weather data again. If the user decides to close the dashboard but come back to it later, it will still contain the last searched city's weather data. 
+
+![Screenshot][weather-dashboard.jpg]
+
+Link: https://nartexyu.github.io/Weather-Dashboard/
--- script.js
+++ script.js
@@ -1,8 +1,41 @@
+// Creates current date
+let d = new Date()
+let date = (d.getMonth()+1) + "-" + d.getDate() + "-" + d.getFullYear();
+//console.log(date)
+
+// Retreives last searched city from local storage runs functions for search
+let lastCity = localStorage.getItem("lastCity")
+if (lastCity != null) {
+    getCurrent(lastCity);
+    getForecast(lastCity);
+};
+
+// Clears previous search history on page load
+$(".history").html("")
+
+// On click event to run ajax functions
 $(".btn").on("click", function() {
     let search = $(".search").val();
-    getCurrent(search)
-})
+    localStorage.setItem("lastCity", search)
+    getCurrent(search);
+    getForecast(search);
+    $(".search").val("");
 
+    $(".history").append(`<div class="col-10 d-flex mb-2 p-2 border cityHistory"> ${search} </div>`)
+
+    // On click event to research previously searched cities
+    $(".cityHistory").on("click", function() {
+        let research = $(this).text()
+        // localStorage.setItem("lastCity", research)
+        console.log(research)
+        getCurrent(research);
+        getForecast(research);
+    });
+});
+
+
+
+// Function to ajax call current weather data
 function getCurrent(search) {
     let currentUrl = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=d26c6650a01058b5807115b5f2832783";
     
@@ -10,16 +43,15 @@ function getCurrent(search) {
         url: currentUrl,
         method: "GET"
     }).then (function(response){
-        console.log(response)
+        // console.log(response)
         let temp = Math.trunc((response.main.temp - 273.15) * 9/5 + 32)
         $(".temperature").text("Weather: " + temp + "\u00B0 F")
         $(".humidity").text("Humidity: " + response.main.humidity + "%")
         $(".wind").text("wind: " + response.wind.speed + " MPH")
 
         $(".city").empty()
-        $(".city").text(response.name)
+        $(".city").text(response.name + " (" + date + ")")
         let emoji = response.weather[0].icon
-        console.log(emoji)
         $(".city").append('<img src="https://openweathermap.org/img/wn/' + emoji + '@2x.png" class="weather-icon">')
         
         let lat = response.coord.lat
@@ -43,6 +75,7 @@ function getCurrent(search) {
     });
 };
 
+// Function to ajax call 5 day weather forecast
 function getForecast(search) {
     let forecastUrl = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=d26c6650a01058b5807115b5f2832783";
     
@@ -51,6 +84,19 @@ function getForecast(search) {
         method: "GET"
     }).then (function(response){
         console.log(response)
+
+        for(let i = 0; i < 40; i=i+8){
+            let date = response.list[i].dt_txt.substring(0,10)
+            $(".date"+i).text(date)
+
+            let emoji = response.list[i].weather[0].icon
+            $(".emoji"+i).attr("src", "http://openweathermap.org/img/wn/" + emoji + ".png")
+
+            let temperature = Math.trunc((response.list[i].main.temp - 273.15) * 9/5 + 32)
+            $(".fWeather"+i).text("Temp: " + temperature + "°F")
+            
+            $(".fHumidity"+i).text("Humidity: " + response.list[i].main.humidity + "%")
+        }
     });
 };
 
